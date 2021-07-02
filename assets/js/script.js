8fa289c1b90f8a39e01a6297aa57691a3840edec
ManavellaA <andresraulmanavella@gmail.com>
2021-07-01 21:35:12 -0300

    sumando al proyecto

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,71 +7,94 @@
 //    return user = nombre;
 //}
 //logUser();
-
+function calcImc(){
     let imc = document.getElementById("btn_imc");
     imc.addEventListener("click", validacion);
-        
-        function validacion(e){
-            e.preventDefault();
 
+        function validacion(){
             let peso = document.getElementById("calc_peso").value;
-            
             let altura = document.getElementById("calc_altura").value;
 
             peso = parseInt(peso);
             altura = parseInt(altura);
-            
-            if( ! peso || peso === " " && ! altura || altura === " " ){
 
-                let calc = Math.round(peso * (Math.sqrt(altura)/100)/10);
+            if( peso > 0 && altura > 0 ){
+                let calc = (peso * ((altura/100) * (altura/100)))/10;
                 let imc = calc.toFixed(2);
-                
-                let contenedor = document.createElement("p");
-                contenedor.setAttribute("id", "div_imc");
-                let dataNueva = document.createTextNode(`Tu IMC es ${imc}`);
-                contenedor.appendChild(dataNueva);
-
-                let getDiv = document.getElementById("div_imc");
-                let nodoPadre = getDiv.parentNode
-                nodoPadre.replaceChild(contenedor, getDiv);
+                //DIV de contenido 
+                let contenedorNuevo = document.createElement("div");
+                contenedorNuevo.setAttribute("id", "div_imc");
+                let contenedorNuevoData = document.createElement("div");
+                contenedorNuevoData.innerHTML = 
+                     `<p>Tu IMC es: <strong style="color: blue">${imc}</strong></p>
+                     <p>Segun la OMS clasificaria como:</p>
+                     <p style="color: blue"><strong>${ImcClasificacion(imc)}</strong></p>`
+                contenedorNuevo.appendChild(contenedorNuevoData);
+                //Intercambio
+                let contenedorViejo = document.getElementById("div_imc");
+                let nodoPadre = contenedorViejo.parentNode
+                nodoPadre.replaceChild(contenedorNuevo, contenedorViejo);
+            }else{
+                alert("No ingresaste todos los campos requeridos");
             }
         }
+}
+calcImc();
+
+function ImcClasificacion(valor){
+    
+    if(valor < 18.5){
+        return "Bajo Peso"
+    }
+    else if(valor < 24.9){   
+        return "Peso Normal"
+    }
+    else if(valor < 29.9){
+        return "Sobrepeso"
+    }
+    else if(valor < 34.9){
+        return "Obesidad Tipo 1"
+    }
+    else if(valor < 39.9){
+        return "Obesidad Tipo 2"
+    }
+    else{
+        return "Obesidad Tipo 3"
+    }
+}
+
+function mensaje(){
+
+    //Al no tener donde enviar el mensaje lo almaceno en el local Storage
+
+    let nombre = document.getElementById("contactoNombre").value;
+    let email = document.getElementById("contactoEmail").value;
+    let asunto = document.getElementById("contactoAsunto").value;
+    let contenido = document.getElementById("contactoMensaje").value;
+
+    class envio{
+        constructor(dato1, dato2, dato3, dato4){
+            this.usuario = dato1;
+            this.email = dato2;
+            this.asunto = dato3;
+            this.contenido = dato4;
+        }
+    }
+
+    const Mensaje = new envio(nombre, email, asunto, contenido)
+
+    
+}
+
+
+
+
+
+
+
 
 //userData();
 //
-//function principal(){
-//let eleccion = prompt(`Si queres sabes tu IMC escribe "IMC", si queres calificar tu Ciclo de Enternamiento escribe "Ciclo"`);
-//eleccion = eleccion.toUpperCase();
-//
-//if(eleccion === "CICLO"){
-//    ciclo();
-//
-//    eleccion = prompt(`Si todavia queres saber tu IMC escribe "IMC", sino solo presiona Enter`);
-//    eleccion = eleccion.toUpperCase();
-//    if(eleccion === "IMC"){
-//    Imc();
-//    }
-//    else{
-//        imc = "N/A"
-//    };
-//}
-//
-//else{
-//    Imc();
-//    eleccion = prompt(`Si todavia queres ingresar tu ciclo de entenamiento escribe "Ciclo", sino solo presiona Enter`);
-//    eleccion = eleccion.toUpperCase();
-//        if(eleccion === "CICLO"){
-//            ciclo();
-//        }
-//        else{
-//            promedio = "N/A";
-//            msj = "N/A";
-//        }
-//}
-//
-//alert(`Gracias por visitar nuestro sitio`);
-//}
-//principal();
 ////------------------------------------
 //
 //function dataCharge(){
@@ -119,63 +142,4 @@
 //    msjAlert = user + " tu promedio es " + promedio + " min/km y tu desempeño clasifica como " + msj;
 //    alert(msjAlert);
 //}
-//
-//function Imc(){
-//    imc = (peso * ((estatura/100) * (estatura/100)))/10;
-//    
-//    if(imc < 18.5){
-//        alert(`Tu IMC es ${imc}Kg/m², Clasifica dentro de "Bajo Peso"`)
-//    }
-//    else if(imc < 24.9){   
-//        alert(`Tu IMC es ${imc}Kg/m², Clasifica dentro de "Peso Normal"`)
-//    }
-//    else if(imc < 29.9){
-//        alert(`Tu IMC es ${imc}Kg/m², Clasifica dentro de "Sobrepeso"`)
-//    }
-//    else if(imc < 34.9){
-//        alert(`Tu IMC es ${imc}Kg/m², Clasifica dentro de "Obesidad Tipo 1"`)
-//    }
-//    else if(imc < 39.9){
-//        alert(`Tu IMC es ${imc}Kg/m², Clasifica dentro de "Obesidad Tipo 2"`)
-//    }
-//    else{
-//        alert(`Tu IMC es ${imc}Kg/m², Clasifica dentro de "Obesidad Tipo 3"`)
-//    }
-//}
-//
-//function storageUsuario(){
-//
-//const arrayUsuario = []
-//
-//class dataUser{
-//    constructor(dato1, dato2, dato3, dato4, dato5, dato6){
-//        this.usuario = dato1;
-//        this.peso = dato2;
-//        this.estatura = dato3;
-//        this.promedio = dato4;
-//        this.desempeño = dato5;
-//        this.imc = Math.fround(dato6);
-//    }
-//    leer(){
-//        console.log(`El usuario ${this.usuario} pesa ${this.peso}Kg, mide ${estatura}cm, su IMC es de ${imc} y su desempeño calsifica como ${this.desempeño}`)
-//    }
-//}
-//
-//arrayUsuario.push(new dataUser(user, peso, estatura, promedio, msj, imc))
-//
-//for (let usuario of arrayUsuario){
-//    let contenedor = document.createElement("div");
-//    contenedor.innerHTML = 
-//    `<br>
-//     <p>Nombre de Usuario: ${usuario.usuario}</p>
-//     <p>Peso: ${usuario.peso}Kg</p>
-//     <p>Estatura: ${usuario.estatura}cm</p>
-//     <p>Promedio: ${usuario.promedio}Km/min</p>
-//     <p>Desempeño deportivo: ${usuario.desempeño}</p>
-//     <p>IMC: ${usuario.imc}</p>
-//     <br>`
-//    document.body.appendChild(contenedor);
-//    usuario.leer();
-//}
-//}
-//storageUsuario();
+
