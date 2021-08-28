eb75638994bf8189fd69be3cbd10f9a9b22eafd0
ManavellaA <andresraulmanavella@gmail.com>
2021-08-28 00:28:09 -0300

    posibles cambios

--- js/raiz.js
+++ js/raiz.js
@@ -1,53 +1,56 @@
 let arrayData = []
-    $(`.btn__calc`).click(function (e) {
-        e.preventDefault();
-        
-        if(validacion_datos() === true){
 
-            console.log("paso la validacion");
-        }else{
-            console.log("NO paso al validacion - se setea el Array");
+$(`.btn__calc`).click(function (e) {
+    e.preventDefault();
+    if(validacion_diodos_de_caida() === true){
 
-        }
-    });
-    
-    function validacion_datos() {
-        let nElementos = $(`.input_elementos`).val()
-        let capBateria = $(`.input_cap_bateria`).val()
-        let vFondo = $(`.input_v_fondo`).val()
-        let vTrabajo = $(`.input_v_trabajo`).val()
-        let rangoCadena = $(`.input_rango_cadena`).val()
+        console.log("validacion correcta");
+        calc_diodos_de_caida();
+    }else{
+        console.log("NO paso al validacion - se setea el ArrayData");
+    }
+});
 
-        if(nElementos !== ""){
-            arrayData.push({nElementos: nElementos})
-            if(capBateria !== ""){
-                arrayData.push({capBateria: capBateria})   
-                if(vFondo !== ""){
-                    arrayData.push({vFondo: vFondo})   
-                    if(vTrabajo !== ""){
-                        arrayData.push({vTrabajo: vTrabajo})
-                        if(rangoCadena !== ""){
-                            arrayData.push({rangoCadena: rangoCadena})
-                        }
+function validacion_diodos_de_caida() {
+    let nElementos = parseInt($(`.input_elementos`).val())
+    let capBateria = parseInt($(`.input_cap_bateria`).val())
+    let vFondo = parseFloat($(`.input_v_f ondo`).val())
+    let vTrabajo = parseFloat($(`.input_v_trabajo`).val())
+    let rangoCadena = parseInt($(`.input_rango_cadena`).val())
+
+    if(nElementos !== ""){
+        arrayData.push({nElementos: nElementos})
+        if(capBateria !== ""){
+            arrayData.push({capBateria: capBateria})   
+            if(vFondo !== ""){
+                arrayData.push({vFondo: vFondo})   
+                if(vTrabajo !== ""){
+                    arrayData.push({vTrabajo: vTrabajo})
+                    if(rangoCadena !== ""){
+                        arrayData.push({rangoCadena: rangoCadena})
                     }
                 }
             }
         }
-
-        if(arrayData.length === 5){
-            $(`.input_elementos`).val("")
-            $(`.input_cap_bateria`).val("")
-            $(`.input_v_fondo`).val("")
-            $(`.input_v_trabajo`).val("")
-            $(`.input_rango_cadena`).val("")
-            return true
-        }
-        else{
-            Swal.fire(
-                'Atención!',
-                `Falta completar un campo`,
-                'warning'
-            )
-            arrayData = []
-        }
     }
+
+    if(arrayData.length === 5){
+        $(`.input_elementos`).val("")
+        $(`.input_cap_bateria`).val("")
+        $(`.input_v_fondo`).val("")
+        $(`.input_v_trabajo`).val("")
+        $(`.input_rango_cadena`).val("")
+        return true
+    }else{
+        Swal.fire(
+            'Atención!',
+            `Falta completar un campo`,
+            'warning'
+        )
+        arrayData = []
+    }
+}
+
+function calc_diodos_de_caida(){
+    
+}
