d55c9830dd84c1b011dfa78a60f7cfa998ae203c
ManavellaA <andresraulmanavella@gmail.com>
2021-08-30 22:34:46 -0300

    add

--- js/raiz.js
+++ js/raiz.js
@@ -12,21 +12,26 @@ $(`.btn__calc`).click(function (e) {
 });
 
 function validacion_diodos_de_caida() {
-    let nElementos = parseInt($(`.input_elementos`).val())
-    let capBateria = parseInt($(`.input_cap_bateria`).val())
-    let vFondo = parseFloat($(`.input_v_f ondo`).val())
-    let vTrabajo = parseFloat($(`.input_v_trabajo`).val())
-    let rangoCadena = parseInt($(`.input_rango_cadena`).val())
+    let nElementos = $(`.input_elementos`).val()
+    let capBateria = $(`.input_cap_bateria`).val()
+    let vFondo = $(`.input_v_fondo`).val()
+    let vTrabajo = $(`.input_v_trabajo`).val()
+    let rangoCadena = $(`.input_rango_cadena`).val()
 
     if(nElementos !== ""){
+        nElementos = parseInt(nElementos);
         arrayData.push({nElementos: nElementos})
         if(capBateria !== ""){
+            capBateria = parseInt(capBateria);
             arrayData.push({capBateria: capBateria})   
             if(vFondo !== ""){
+                vFondo = parseFloat(vFondo);
                 arrayData.push({vFondo: vFondo})   
                 if(vTrabajo !== ""){
+                    vTrabajo = parseInt(vTrabajo);
                     arrayData.push({vTrabajo: vTrabajo})
                     if(rangoCadena !== ""){
+                        rangoCadena = parseInt(rangoCadena);
                         arrayData.push({rangoCadena: rangoCadena})
                     }
                 }
